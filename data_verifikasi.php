e6b2db5558d0f08a5a4db24915db45e8a92c5a45
CiciIrmayanti <55769697+CiciIrmayanti@users.noreply.github.com>
2020-10-16 09:42:03 +0700

    view

--- data_verifikasi.php
+++ data_verifikasi.php
@@ -1,127 +1,204 @@
-<?php
+<div class="container" style="margin-top: 60px;">
+		<a href="#form" data-toggle="modal" class="btn btn-primary" onclick="submit('tambah')">Tambah Data</a>
+        <a href="<?php echo base_url('admin/data_verifikasi/cetak')?>" class="btn btn-warning">Cetak PDF</a>
+        <a href="<?php echo base_url('admin/data_verifikasi/cetak_xls')?>" class="btn btn-success">Cetak XLS</a>
+        <br>
+		<div class="col-md-12">
+        <br>
+			<table id="data" class="table table-striped table-bordered" cellspacing="0" width="100%">
+				<thead>
+					<tr>
+						<td scope="col">Id Tanggapan</td>
+						<td scope="col">ID Pengaduan</td>
+						<td scope="col">Tanggal Tanggapan</td>
+						<td scope="col">Tulis Tanggapan</td>
+						<td scope="col">ID Petugas</td>
+                        <td scope="col">Aksi</td>
+					</tr>
+				</thead>
+				<tbody id="target">
 
-class Data_verifikasi extends CI_Controller
-{
-    public function index()
-    {
-            $this->load->view('templates_admin/header');
-            $this->load->view('templates_admin/sidebar');
-            $this->load->view('admin/data_verifikasi');
-            $this->load->view('templates_admin/footer');
-    }
+				</tbody>
+			</table>
+		</div>
+	</div>
+	<div class="modal fade" id="form" role="dialog">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h1>Data Tanggapan</h1>
+				</div>
+               <center><font color=red><p id="pesan"></p></font></center>
+				<table class="table">
+					<tr>
+						<td>Id Pengaduan</td>
+						<td>
+                            <input type="text" name="id_pengaduan" placeholder="Id Pengaduan" class="form-control">
+                            <input type="hidden" name="id_tanggapan" value="">
+                        </td>
+					</tr>
+					<tr>
+						<td>Tanggal Tanggapan</td>
+						<td>
+                            <input type="text" name="tgl_tanggapan" placeholder="Tanggal Tanggapan" class="form-control">
+                        </td>
+					</tr>
+					<tr>
+						<td>Tulis Tanggapan</td>
+						<td>
+                            <input type="text" name="tanggapan" placeholder="Tulis Tanggapan" class="form-control">
+                        </td>
+					</tr>
+					<tr>
+						<td>Id Petugas</td>
+						<td>
+                            <input type="text" name="id_petugas" placeholder="Id Petugas" class="form-control">
+						</td>
+					</tr>
+					<tr>
+						<td></td>
+						<td>
+                            <button type="button" id="btn-tambah" onclick="tambahdata()" class="btn btn-success">Tambah</button>
+                            <button type="button" id="btn-ubah" onclick="ubahdata()" class="btn btn-success">Ubah</button>
+                            <button type="button" data-dismiss="modal" class="btn btn-danger">Batal</button>
+                        </td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	</div>
 
-    public function cetak(){
-        ob_start();
-        $data['c_tanggapan'] = $this->model_verifikasi->count_tanggapan();
-        $data['tanggapan'] = $this->model_verifikasi->get_tanggapan();
-        $this->load->view('admin/preview_tanggapan',$data);
-        $html = ob_get_contents();
-                ob_end_clean();
-        require './assets/html2pdf/autoload.php';
+	<script type="text/javascript">
+		ambilData();
 
-        $pdf = new Spipu\Html2Pdf\Html2Pdf('P','A4','en');
-        $pdf->WriteHTML($html);
-        $pdf->Output('Data_Tanggapan_'.date('d-m-Y'). '.pdf', 'D');
-    }
-
-    public function multimedia(){
-        $this->load->view('base');
-    }
-
-    public function cetak_xls(){
-        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        header('Content-Disposition: attachment; filename="Data Tangggapan.xls"');
-        header('Cache-Control: max-age=0');
-        $data['c_tanggapan'] = $this->model_verifikasi->count_tanggapan();
-        $data['tanggapan'] = $this->model_verifikasi->get_tanggapan();
-        $this->load->view('admin/preview_tanggapan', $data);
-    }
-
-    public function ambilData()
-    {
-        $dataTanggapan = $this->model_verifikasi->ambilData('tanggapan')->result();
-        echo json_encode($dataTanggapan);
-    }
-
-    public function tambahdata()
-    {
-        $id_pengaduan=$this->input->post('id_pengaduan');
-        $tgl_tanggapan=$this->input->post('tgl_tanggapan');
-        $tanggapan=$this->input->post('tanggapan');
-        $id_petugas=$this->input->post('id_petugas');
-
-        if($id_pengaduan==''){
-            $result['pesan']= "Id Pengaduan Harus Diisi";
-        } else if($tgl_tanggapan == '') {
-            $result['pesan'] = "Tanggal Harus Diisi";
-        } else if($tanggapan == '') {
-            $result['pesan'] = "Harus Diisi";
-        } else if($id_petugas == ''){
-            $result['pesan'] = "Harus Diisi";
-        } else  {
-            $result['pesan'] = "";
-
-        $data = array(
-            'id_pengaduan' => $id_pengaduan,
-            'tgl_tanggapan' => $tgl_tanggapan,
-            'tanggapan' => $tanggapan,
-            'id_petugas' => $id_petugas
-        );
-
-            $this->model_verifikasi->tambahdata($data,'tanggapan');
-           
+		function ambilData() {
+			$.ajax({
+				type: 'POST',
+				url: '<?php echo base_url()."admin/data_verifikasi/ambilData" ?>',
+				dataType: 'json',
+				success: function (data) {
+					var baris = '';
+					for (var i = 0; i < data.length; i++) {
+						baris += '<tr>' +
+							'<td>' + data[i].id_tanggapan + '</td>' +
+							'<td>' + data[i].id_pengaduan + '</td>' +
+							'<td>' + data[i].tgl_tanggapan + '</td>' +
+							'<td>' + data[i].tanggapan + '</td>' +
+                            '<td>' + data[i].id_petugas + '</td>' +
+                            '<td><a href="#form" data-toggle="modal" class="btn btn-primary" onclick="submit('+data[i].id_tanggapan+')">Edit</a> <a onclick="hapusdata('+data[i].id_tanggapan+')" class="btn btn-danger">Hapus</a> <a class="btn btn-success">Verifikasi</a> </td>' +
+							'</tr>';
+					}
+					$('#target').html(baris);
+				}
+			});
         }
-        echo json_encode($result);
-       
-    }
-    public function ubahdata()
-    {
-        $id_tanggapan=$this->input->post('id_tanggapan');
-        $id_pengaduan=$this->input->post('id_pengaduan');
-        $tgl_tanggapan=$this->input->post('tgl_tanggapan');
-        $tanggapan=$this->input->post('tanggapan');
-        $id_petugas=$this->input->post('id_petugas');
+        
+        function tambahdata()
+        {
+            var id_pengaduan=$("[name='id_pengaduan']").val();
+            var tgl_tanggapan=$("[name='tgl_tanggapan']").val();
+            var tanggapan=$("[name='tanggapan']").val();
+            var id_petugas=$("[name='id_petugas']").val();
 
-        if($id_pengaduan==''){
-            $result['pesan']= "Id Pengaduan Harus Diisi";
-        } else if($tgl_tanggapan == '') {
-            $result['pesan'] = "Tanggal Harus Diisi";
-        } else if($tanggapan == '') {
-            $result['pesan'] = "Harus Diisi";
-        } else if($id_petugas == ''){
-            $result['pesan'] = "Harus Diisi";
-        } else  {
-            $result['pesan'] = "";
+            $.ajax({
+                type: 'POST',
+                data: 'id_pengaduan=' +id_pengaduan +
+                '&tgl_tanggapan='+ tgl_tanggapan+
+                '&tanggapan='+ tanggapan+
+                '&id_petugas='+ id_petugas,
 
-            $where = array('id_tanggapan'=>$id_tanggapan);
-            
-            $data = array(
-            'id_pengaduan' => $id_pengaduan,
-            'tgl_tanggapan' => $tgl_tanggapan,
-            'tanggapan' => $tanggapan,
-            'id_petugas' => $id_petugas
-        );
+                url: '<?php echo base_url("admin/data_verifikasi/tambahdata")?>',
+                dataType: 'json',
+                success: function(hasil){
+                    $('#pesan').html(hasil.pesan);
 
-            $this->model_verifikasi->updatedata($where, $data,'tanggapan');
-           
+                    if (hasil.pesan == '') {
+                        $('#form').modal('hide');
+                        ambilData();
+
+                        $("[name='id_pengaduan']").val('');
+                        $("[name='tgl_tanggapan']").val('');
+                        $("[name='tanggapan']").val('');
+                        $("[name='id_petugas']").val('');
+
+                    }
+                }
+            });
         }
-        echo json_encode($result);
-       
-    }
 
-    public function ambilId()
-    {
-        $id_tanggapan = $this->input->post('id_tanggapan');
-        $where=array('id_tanggapan'=>$id_tanggapan);
-        $datatanggapan = $this->model_verifikasi->ambilId('tanggapan', $where)->result();
+        function submit(x)
+        {
+            if(x=='tambah')
+            {
+                $('#btn-tambah').show();
+                $('#btn-ubah').hide();
+            } else {
+                $('#btn-tambah').hide();
+                $('#btn-ubah').show();
 
-        echo json_encode($datatanggapan);
-    }
 
-    public function hapusdata()
-    {
-        $id_tanggapan = $this->input->post('id_tanggapan');
-        $where = array('id_tanggapan'=>$id_tanggapan);
-        $this->model_verifikasi->hapusdata($where,'tanggapan');
-    }
-}
+                $.ajax({
+                    type: 'POST',
+                    data: 'id_tanggapan='+x,
+                    url: '<?php echo base_url().'admin/data_verifikasi/ambilId'?>',
+                    dataType: 'json',
+                    success: function(hasil){
+                        $("[name='id_tanggapan']").val(hasil[0].id_tanggapan);
+                        $("[name='id_pengaduan']").val(hasil[0].id_pengaduan);
+                        $("[name='tgl_tanggapan']").val(hasil[0].tgl_tanggapan);
+                        $("[name='tanggapan']").val(hasil[0].tanggapan);
+                        $("[name='id_petugas']").val(hasil[0].id_petugas);
+                    }
+                })
+
+            }
+        }
+
+        function ubahdata()
+        {
+            var id_tanggapan=$("[name='id_tanggapan']").val();
+            var id_pengaduan=$("[name='id_pengaduan']").val();
+            var tgl_tanggapan=$("[name='tgl_tanggapan']").val();
+            var tanggapan=$("[name='tanggapan']").val();
+            var id_petugas=$("[name='id_petugas']").val();
+
+
+            $.ajax({
+                type: 'POST',
+                data:  'id_tanggapan=' +id_tanggapan +
+                '&id_pengaduan=' +id_pengaduan +
+                '&tgl_tanggapan='+ tgl_tanggapan+
+                '&tanggapan='+ tanggapan+
+                '&id_petugas='+ id_petugas,
+                url: '<?php echo base_url().'admin/data_verifikasi/ubahdata' ?>',
+                dataType: 'json',
+                success: function (hasil) {
+                    $('#pesan').html(hasil.pesan);
+
+                    if (hasil.pesan == '') {
+                        $('#form').modal('hide');
+                        ambilData();
+                    }
+                }
+            });
+        }
+
+        function hapusdata(id_tanggapan)
+        {
+            var tanya = confirm('Apakah anda yakin akan menghapus data?');
+
+            if(tanya)
+            {
+                $.ajax({
+                    type: 'POST',
+                    data: 'id_tanggapan='+id_tanggapan,
+                    url: '<?php echo base_url()."admin/data_verifikasi/hapusdata" ?>',
+                    success: function()
+                    { 
+                        ambilData();
+                     }
+                });
+            }
+        }
+
+	</script>
